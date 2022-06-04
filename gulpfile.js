15f10a9689e94072233bd098a1c67151e0f9c4b3
Kate <noskova2111@mail.ru>
2022-06-04 12:29:06 +0300

    setting of the folder dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const webpack = require("webpack-stream");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require("autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const postcss = require("gulp-postcss");
@@ -19,7 +19,7 @@ gulp.task("build-js", () => {
                 .pipe(webpack({
                     mode: 'development',
                     output: {
-                        filename: 'script.js'
+                        filename: 'main.js'
                     },
                     watch: false,
                     devtool: "source-map",
