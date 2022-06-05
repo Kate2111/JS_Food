e4ed4de336a47c7513bfc17ec5f1ba8dbdf8ea66
Kate <noskova2111@mail.ru>
2022-06-05 20:01:00 +0300

    tabcontent jpg

--- src/js/main.js
+++ src/js/main.js
@@ -42,17 +42,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Timer
 
-    const deadline = '2022-05-08';
+    const deadline = '2022-07-31';
 
-    function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date());
-        const days = Math.floor(t / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    function getTimeRemaining(endtime) { //задача функции получить разницу между датам
+        //Date.parse(endtime) - получим колво милисекнд планируемой даты
+        //Date.parse(new Date()) - текущая дата
+        //Math.floor - округление до ближайшего целого
+        const t = Date.parse(endtime) - Date.parse(new Date()); 
+        const days = Math.floor(t / (1000 * 60 * 60 * 24)); // получаем сколько в сутках милисекунд
+        const hours = Math.floor((t / (1000 * 60 * 60)) % 24); //% - возвращает остаток от деления
         const minutes = Math.floor((t / 1000 / 60) % 60);
         const seconds = Math.floor((t / 1000) % 60);
 
         return {
-            'total': t,
+            'total': t, //показывает общее количество милисекунд
             days, //'days': days, старый синтаксис
             hours, //'hours': hours,
             minutes, //'minutes': minutes,
@@ -60,7 +63,7 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     }
     
-    function getZero(num) { //функция для добавления нуля пере числом, если число меньше 10, например 08,09
+    function getZero(num) { //функция для добавления нуля перед числом, если число меньше 10, например 08,09
         if (num >= 0 && num < 10) {
             return `0${num}`;
         } else {
@@ -68,18 +71,18 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function setClock(selector, endtime) {
+    function setClock(selector, endtime) { //функция выводит таймер на страницу
         const timer = document.querySelector(selector);
         const days = timer.querySelector('#days');
         const hours = timer.querySelector('#hours');
         const minutes = timer.querySelector('#minutes');
         const seconds = timer.querySelector('#seconds');
-        const timeInterval = setInterval(updateClock, 1000);
+        const timeInterval = setInterval(updateClock, 1000); //запускаем таймер через каждую секунду
 
-        updateClock();
+        updateClock(); //запускаем функцию вручную, чтобы устранить мегание
 
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
+        function updateClock() { // Обновление часов , расчет времени на данную секунду 
+            const t = getTimeRemaining(endtime); // разница между текущим и планируемым временем
 
             days.innerHTML = getZero(t.days);
             hours.innerHTML = getZero(t.hours);
