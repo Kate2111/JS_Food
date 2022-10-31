184d83fcc96c6b04e5db3e8bc1283632369c32ab
Kate <noskova2111@mail.ru>
2022-10-31 22:31:42 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -109,7 +109,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const modalTrigger = document.querySelectorAll('[data-modal]');
   const modal = document.querySelector('.modal');
-  const modalCloseBtn = document.querySelector('[data-close]'); // т.к. код ниже используется два раза и более, нам необъодимо его вынести в отдельную функцию
+  /* const modalCloseBtn = document.querySelector('[data-close]'); */
+  //в уроке 54 удаляем переменную, так каксоздаем закрытие окна для динамически созданного элемента
+  // т.к. код ниже используется два раза и более, нам необъодимо его вынести в отдельную функцию
 
   /* 
   modalTrigger.forEach(btn => {
@@ -132,7 +134,7 @@ window.addEventListener('DOMContentLoaded', () => {
   modalTrigger.forEach(btn => {
     btn.addEventListener('click', openModal);
   });
-  const modalTimerId = setTimeout(openModal, 3000); //урок 44
+  const modalTimerId = setTimeout(openModal, 50000); //урок 44
   // т.к. код ниже используется два раза и более, нам необъодимо его вынести в отдельную функцию
 
   /* 
@@ -156,12 +158,13 @@ window.addEventListener('DOMContentLoaded', () => {
     modal.classList.remove('show');
     document.body.style.overflow = '';
   }
+  /* modalCloseBtn.addEventListener('click', closeModal);  */
+  // функцию closeModal мы не вызываем! а только передаем!  //в уроке 54 удаляем
 
-  modalCloseBtn.addEventListener('click', closeModal); // функцию closeModal мы не вызываем! а только передаем! 
 
   modal.addEventListener('click', e => {
     // функция, чтобы модальное окно закрывалось по клику на подложку
-    if (e.target === modal) {
+    if (e.target === modal || e.target.getAttribute('data-close') == '') {
       closeModal(); // тут функцию closeModal вызываем, т.к. нам нужно ее выполнить после условия!
     }
   });
@@ -180,7 +183,231 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  window.addEventListener('scroll', showModalByScroll); // Используем классы для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+
+      for (var _len = arguments.length, classes = new Array(_len > 6 ? _len - 6 : 0), _key = 6; _key < _len; _key++) {
+        classes[_key - 6] = arguments[_key];
+      }
+
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 58;
+      this.changeToRUS();
+    }
+
+    changeToRUS() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.classes = 'menu__item';
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена</div>
+                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                </div>
+            `;
+      this.parent.append(element);
+    }
+
+  }
+
+  getResource('http://localhost:3000/menu').then(data => {
+    data.forEach(_ref => {
+      let {
+        img,
+        altimg,
+        title,
+        descr,
+        price
+      } = _ref;
+      new MenuCard(img, altimg, title, descr, price, '.menu.container').render();
+    });
+  }); //Урок 53
+  // Создание форм и отправка данных на сервер 
+  // Forms
+
+  const forms = document.querySelectorAll('form');
+  const message = {
+    loading: 'icons/veg.svg',
+    success: 'Спасибо! Скоро мы свяжемся с вами',
+    failure: 'Что-то пошло не так...'
+  };
+  forms.forEach(item => {
+    bindPostData(item);
+  });
+
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+    return await res.json();
+  };
+
+  async function getResource(url) {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  } //функция которая отвечает за постинг данных
+
+
+  function bindPostData(form) {
+    form.addEventListener('submit', e => {
+      e.preventDefault(); // отменяем стандартное поведение браузера, нужно прописывать в начале 
+
+      const statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+      form.insertAdjacentElement('afterend', statusMessage);
+      /*  //убираем, так как будем использовать fetch
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php'); //сначала вызываем метод open, чтобы настроить наш запрос
+       */
+
+      const formData = new FormData(form);
+      /* const object = {};
+      formData.forEach(function(value, key) {
+          object[key] = value;
+      }); */
+
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+      postData('http://localhost:3000/requests', json).then(data => {
+        console.log(data);
+        showThanksModal(message.success);
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(message.failure);
+      }).finally(() => {
+        form.reset();
+      });
+    });
+  } //Урок 54. Красивое оповещение пользователя
+
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+    prevModalDialog.classList.add('hide');
+    openModal(); //создаем блок и присваеваем классы
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+            <div class="modal__content">                  
+                <div data-close class="modal__close">&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    }, 4000);
+  } // Урок 56. Fetch API and promice
+
+  /* fetch('http://localhost:3000/menu', {
+      method: "POST",
+      body: JSON.stringify({name: 'Alex'}),
+      headers: {
+          'Content-type': 'applicatiom/json'
+      }
+  })
+      .then(response => response.json())
+      .then(json => console.log(json)) */
+  // slider
+  // Урок 61 - 1 вариант
+
+  /* 
+  const slides = document.querySelectorAll('.offer__slide');
+  const prev = document.querySelector('.offer__slider-prev');
+  const next = document.querySelector('.offer__slider-next');
+  const total = document.querySelector('#total');
+  const current  = document.querySelector('#current');
+  let slideIndex = 1; 
+    showSlides(slideIndex);
+    if (slides.length < 10) {
+      total.textContent = `0${slides.length}`;
+  } else { 
+      total.textContent = slides.length;
+  }
+    function showSlides(n)  {
+      if (n > slides.length) {
+          slideIndex = 1;
+      }
+        if (n < 1) {
+          slideIndex = slides.length;
+      }
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+        if (slides.length < 10) {
+          current.textContent = `0${slideIndex}`;
+      } else { 
+          current.textContent = slideIndex;
+      } 
+  }
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+    next.addEventListener('click', () => {
+      plusSlides(1);
+  });
+    prev.addEventListener('click', () => {
+      plusSlides(-1);
+  });
+  */
+  //Урок 62 - 2 вариант
+  // В этом варианте, дополнительно добавили обертку "offer__slider-inner" в html 
+
+
+  const slides = document.querySelectorAll('.offer__slide');
+  const prev = document.querySelector('.offer__slider-prev');
+  const next = document.querySelector('.offer__slider-next');
+  const total = document.querySelector('#total');
+  const current = document.querySelector('#current');
+  const slidesWrapper = document.querySelector('.offer__slider-wrapper'); //назначим свойство , это значит, что все что не подходит под ширину этого блока, будет скрыто и невидимо для пользователя 
+
+  const slidesField = document.querySelector('.offer__slider-inner');
+  const width = window.getComputedStyle(slidesWrapper).width; // будем использовать Computed styles - примененные стили от css, их можем получать при помощи скриптов
+
+  let slideIndex = 1; //Устанавливаем блоку ширину,чтобы мы могли полностью поместить все слайды в slidesField
+
+  slidesField.style.width = 100 * slides.length + '%'; //используем %,так как прописываем css стили
+
+  slides.forEach(slide => {
+    slide.style.width = width; //устанавливаем ширину каждому отдельному слайду
+  });
 });
 /******/ })()
 ;
