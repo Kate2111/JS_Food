7030b56917c8d748b0a855f867dfe6e6114f580a
Kate <noskova2111@mail.ru>
2022-11-05 23:11:13 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,34 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/js/script.js',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist/js'
   },
   watch: true,
-
   devtool: "source-map",
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
 
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
